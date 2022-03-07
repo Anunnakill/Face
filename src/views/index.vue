88bad96fb4d1bb16056edeb26cf50532ab82f997
纪成 <eleven@jichengdeMBP.lan>
2022-03-08 01:29:32 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+let flys: NodeListOf<HTMLImageElement>;
+
+const mousemove = (evt: any) => {
+  flys.forEach(it => {
+    let x = it.getBoundingClientRect().left + it.clientWidth / 2;
+    let y = it.getBoundingClientRect().top + it.clientHeight / 2;
+
+    let m = 0;
+    let radian = Math.atan2(evt.x - x, evt.y - y);
+    let rot = radian * (180 / Math.PI) * -1 + m;
+    let rotate = `rotate(${rot}deg)`;
+
+    it.style.transform = rotate;
+  });
+};
+
+const init = () => {
+  flys = document.querySelectorAll("img");
+  window.addEventListener("mousemove", mousemove, false);
+};
+
+onMounted(init);
+</script>
 
 <template>
-  <div id="index"></div>
+  <div id="index">
+    <img src="@/assets/images/fly1.png" />
+    <img src="@/assets/images/fly2.png" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 #index {
   flex: 1;
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  img {
+    width: 300px;
+  }
 }
 </style>
